b2e8cfca62ab36e842371b541bbb95618983a18d
israhman06 <122947069+israhman06@users.noreply.github.com>
2023-03-03 15:29:47 -0800

    Create grove_sensors.py

---
+++ grove_sensors.py
@@ -0,0 +1,65 @@
+import time
+import grovepi
+from grove_rgb_lcd import *
+
+# Connect the Grove Rotary Angle Sensor to analog port A0
+# SIG,NC,VCC,GND
+potentiometer = 0
+
+# Connect the LED to digital port D5
+# SIG,NC,VCC,GND
+led = 5
+
+grovepi.pinMode(potentiometer,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
+time.sleep(1)
+
+# Reference voltage of ADC is 5v
+adc_ref = 5
+
+# Vcc of the grove interface is normally 5v
+grove_vcc = 5
+
+# Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+full_angle = 1023
+
+# set I2C to use the hardware bus
+grovepi.set_bus("RPI_1")
+
+# Connect the Grove Ultrasonic Ranger to digital port D4
+# SIG,NC,VCC,GND
+ultrasonic_ranger = 4
+
+setRGB(118,238,198)
+
+while True:
+    try:
+        # Read sensor value from potentiometer
+        sensor_value = grovepi.analogRead(potentiometer)
+
+        # Calculate voltage
+        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+
+        # Calculate rotation in degrees (0 to 1023)
+        degrees = round((voltage * full_angle) / grove_vcc, 2)
+
+        # Calculate LED brightess (0 to 255) from degrees (0 to 300)
+        brightness = int(degrees / full_angle * 255)
+
+        # Give PWM output to LED
+        grovepi.analogWrite(led,brightness)
+
+        print("sensor_value = %d voltage = %.2f degrees = %.1f brightness = %d" %(sensor_value, voltage, degrees, brightness))
+    except KeyboardInterrupt:
+        grovepi.analogWrite(led,0)
+        break
+    except IOError:
+        print ("Error")
+
+        # Read distance value from Ultrasonic
+        print(grovepi.ultrasonicRead(ultrasonic_ranger))
+
+    except Exception as e:
+        print ("Error:{}".format(e))
+    
+    time.sleep(0.1) # don't overload the i2c bus
